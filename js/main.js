fce0c2af7706c02e4f5de4a5a60bb874a4fa531b
denesnori <denes.nori@yahoo.com>
2017-02-23 13:14:37 +0000

    add initial scrolling effect to header

--- js/main.js
+++ js/main.js
@@ -1,22 +1,53 @@
 (function(){
 
+  //works
   function onScroll(){
     window.addEventListener("scroll",function(){
       document.body.classList[
         window.scrollY > 50 ? "add" : "remove"
       ]('on-scroll');
-      console.log("scrolled");
+    //  console.log("scrolled");
     });
   };
 
-  function visitNavLinks(){
 
+//ok
+  function smoothScrolling(element,position,time){
+    if (time<= 0) {
+      console.log(element,position,time);
+      return;}
+    let distance = position - element.scrollTop;
+
+    let step = distance / time *10;
+    setTimeout(function(){
+      element.scrollTop += step;
+      smoothScrolling(element,position,time-10);
+    },10);
   };
+
+
+
+// aaaaa the click events trigger all 4 options why...???
+  function addScrollingToNavLinks(){
+       document.querySelector(".navbar__list").addEventListener("click", (e)=> {
+         event.preventDefault();
+         if(e.target && e.target.nodeName == "A") {
+           let link = e.target;
+           let target =  document.getElementById(link.getAttribute("href").slice(1));
+     //      console.log(target);
+           let element = navigator.userAgent.indexOf("Firefox") > -1 ? document.documentElement : document.body;
+   //        console.log(element);
+           smoothScrolling(element, target.offsetTop, 800);
+         }
+       });
+};
+
+
 
   window.onload = function(){
     /*Change the opacity of the header on scrolling*/
     onScroll();
     /*Scrolling effect for navbar links*/
-  //  visitNavLinks();
+    addScrollingToNavLinks();
   }
 })()
