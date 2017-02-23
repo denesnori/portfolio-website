444639a33e8ea80757531141bc3b12b721d73685
denesnori <denes.nori@yahoo.com>
2017-02-23 11:58:15 +0000

    add onScroll effect to navbar

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
 (function(){
-  
+
+  function onScroll(){
+    window.addEventListener("scroll",function(){
+      document.body.classList[
+        window.scrollY > 50 ? "add" : "remove"
+      ]('on-scroll');
+      console.log("scrolled");
+    });
+  };
+
+  function visitNavLinks(){
+
+  };
+
+  window.onload = function(){
+    /*Change the opacity of the header on scrolling*/
+    onScroll();
+    /*Scrolling effect for navbar links*/
+  //  visitNavLinks();
+  }
 })()
