2fc3bfb03c1c212696479a62b61cf19bd809887a
denesnori <denes.nori@yahoo.com>
2017-02-25 00:20:32 +0000

    #20 css updates

--- test/e2e/tests.js
+++ test/e2e/tests.js
@@ -4,7 +4,12 @@ module.exports = {
   'Assert Elements': function(browser) {
     browser
       .url("file:///home/nori/projects/portfolio-website/index.html")
+      .waitForElementVisible('body')
+      .saveScreenshot("test/screenshot.png")
       .assert.title('Nora Denes | Web Developer | Portfolio | Contact')
+      .assert.attributeContains('#report-content-source', 'href', 'https://github.com/FAC9/childline-yoti/')
+      .assert.attributeContains('#report-content-online', 'href', 'https://childline-yoti.herokuapp.com/')
+
       .end()
   }
 }
